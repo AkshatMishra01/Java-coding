034f8e73a7abdbe21cac54fc8e66f8194dfde14c
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-01-23 10:28:06 +0530

    Table Program in java

---
+++ table.java
@@ -0,0 +1,11 @@
+class table{
+
+public  static  void  main(String[] args){
+System.out.println("Table of 3 is as follows:");
+int i = 1,num = 3;
+for(i=1;i<=10;i++)
+{
+System.out.println("num *" + i +"=" + num*i);
+}
+}
+}
