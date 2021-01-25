2badd9402815e6c0964b9967a1ce80ece92ba679
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-01-25 10:35:26 +0530

    Create package.java

---
+++ package.java
@@ -0,0 +1,14 @@
+//This is how to create a package in java
+
+package hello; // use the package keyword in java to start making your package
+
+public class hi{
+public static void main(Sting[] args)
+{
+System.out.println("Hello World");
+}
+}
+
+// after this , use the following command in command line / cmd to create a working package that u can use even in your codes further 
+// 1 javac -d directory javafilename (in this case hi)
+// 2 javac -d . hi.java
