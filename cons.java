0a2895be843fbae87857d537ba296817ee4151c0
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-01-27 10:23:34 +0530

    Create cons.java

---
+++ cons.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class cons{
+
+cons()
+{System.out.println("HI,  i'm a constructor");}
+
+
+public static void main(String[] args)
+{
+cons o1 = new cons();
+System.out.println("and HI, I'm a main function");
+}
+}
