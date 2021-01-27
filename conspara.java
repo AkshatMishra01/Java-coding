dfa82714e9948bb8d6cef39308ca38fcd52c26f6
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-01-27 10:42:32 +0530

    Create conspara.java

---
+++ conspara.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+
+class cons{
+
+int num;
+
+cons(int a)
+{
+num = a;
+System.out.println("Num = " + num);
+}
+public static void main(String[] args)
+{
+cons o1 = new cons(3);
+}
+}
