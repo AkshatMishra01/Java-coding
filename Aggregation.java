470e3e3ee6898ac9eca6bb75978e5137398c5329
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-06 09:27:38 +0530

    Aggregation in java

---
+++ Aggregation.java
@@ -0,0 +1,12 @@
+// pseudocode for Aggregation in java
+//Team
+public class Team {      
+   //players can be 0 or more
+   private List players;
+
+   public Car () {
+      players = new ArrayList();
+   }
+}
+//Player Object
+class Player {} 
