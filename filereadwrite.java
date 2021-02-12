d1d5bb6887ab1b9fc195c26ddaa8431fda40181f
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-02-12 09:07:53 +0530

    Create filereadwrite.java

---
+++ filereadwrite.java
@@ -0,0 +1,2 @@
+Java FileWriter and FileReader classes are used to write and read data from text files (they are Character Stream classes). 
+It is recommended not to use the FileInputStream and FileOutputStream classes if you have to read and write any textual information as these are Byte stream classes.
