ce93c7f3f1fc1ecbaa6dcf62c1c3be2ba3ab0856
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-03-02 09:20:20 +0530

    Fields in java

---
+++ fields.java
@@ -0,0 +1,7 @@
+// Fields hold data.
+// Fields can be a Primitive Type or a Reference Type. 
+// There are 8 primitive types in java, which are int, byte, long, short, double, float, boolean, and char.
+// To define a field, we need the type of field, the name of the field, and the value of that field.
+// For example:
+
+int num = 1;
