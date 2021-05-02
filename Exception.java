c628e5031108e2b82777a774fea88d2f1ecea90f
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-05-02 10:19:04 +0530

    Update Exception.java

--- Exception.java
+++ Exception.java
@@ -1,7 +1,7 @@
 public class JavaExceptionExample{  
   public static void main(String args[]){  
    try{  
-      //code that may raise exception  
+      
       int data=100/0;  
    }catch(ArithmeticException e){System.out.println(e);}  
    //rest code of the program   
